98388ce3028c9743b87ac6484268a1da17575231
dominic6809 <muuomusyoki018@gmail.com>
2024-09-30 16:37:11 +0000

    update solved pascals triangle code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,25 +9,25 @@ def pascal_triangle(n):
     Generate Pascal's Triangle up to the nth row.
 
     Args:
-        n (int): Number of rows for Pascal's Triangle. Must be a positive integer.
+        n (int): Number of rows for Pascal's Triangle(positive integer)
 
     Returns:
-        list: A list of lists, where each inner list represents a row in Pascal's Triangle.
+        list: A list of lists, where each inner list represents a row.
               If n <= 0, returns an empty list.
     """
-    
+
     if n <= 0:
         return []
-    
+
     triangle = []
-    
+
     for i in range(n):
         # Initialize a row with 1s, of length i+1
         row = [1] * (i + 1)
-        
+
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
 
         triangle.append(row)
-    
+
     return triangle
